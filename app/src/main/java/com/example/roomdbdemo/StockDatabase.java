027621a6849a248839b54d4ae60088cc7c2c267b
“Eric <enguyen2@yahoo.com>
2020-08-08 21:19:11 -0700

    setup color | fix annotation bug

--- app/src/main/java/com/example/roomdbdemo/StockDatabase.java
+++ app/src/main/java/com/example/roomdbdemo/StockDatabase.java
@@ -3,7 +3,7 @@ package com.example.roomdbdemo;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = (Stock.class), version = 1, exportSchema = false)
+@Database(entities = {Stock.class}, version = 1, exportSchema = false)
 public abstract class StockDatabase extends RoomDatabase {
 
     public abstract StockDAO stockDAO();
