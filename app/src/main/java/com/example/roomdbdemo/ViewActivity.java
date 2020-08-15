b2a8490352c3818d8d43acaa6e15cba3978e3810
“Eric <enguyen2@yahoo.com>
2020-08-14 21:11:23 -0700

    add search data field and functionality

--- app/src/main/java/com/example/roomdbdemo/ViewActivity.java
+++ app/src/main/java/com/example/roomdbdemo/ViewActivity.java
@@ -7,16 +7,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ViewActivity extends AppCompatActivity {
 
     private static RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private static RecyclerView recyclerView;
-    ArrayList<Stock> stockArrayList;
+    ArrayList<Stock> stockArrayList, stockArrayList_search;
+    EditText edt_search;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,25 @@ public class ViewActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        edt_search = findViewById(R.id.edt_search);
+        edt_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String text = charSequence.toString();
+                Filter(text);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         new LoadDataTask().execute();
     }
@@ -47,9 +73,11 @@ public class ViewActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             stockList = stockRepository.getStocks();
             stockArrayList = new ArrayList<>();
+            stockArrayList_search = new ArrayList<>();
 
             for (int i = 0; i < stockList.size(); i++) {
                 stockArrayList.add(stockList.get(i));
+                stockArrayList_search.add(stockList.get(i));
             }
             
             return null;
@@ -59,9 +87,33 @@ public class ViewActivity extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            CustomAdapter customAdapter = new CustomAdapter(stockArrayList, ViewActivity.this);
+            customAdapter = new CustomAdapter(stockArrayList, ViewActivity.this);
             recyclerView.setAdapter(customAdapter);
         }
+    }
+
+    // Filter data
+    public void Filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        Log.d("filter", charText + "");
+
+        stockArrayList.clear();
+
+        if (charText.length() == 0) {
+            stockArrayList.addAll(stockArrayList_search);
+            Log.d("load data", "All");
+        }
+        else {
+            Log.d("load data", "Filtered");
+
+            for (Stock stock:stockArrayList_search) {
+                if (stock.ticker.toLowerCase(Locale.getDefault()).contains(charText)
+                || String.valueOf(stock.quantity).toLowerCase(Locale.getDefault()).contains(charText)) {
+                    stockArrayList.add(stock);
+                }
+            }
+        }
+        customAdapter.notifyDataSetChanged();
     }
 
     @Override
