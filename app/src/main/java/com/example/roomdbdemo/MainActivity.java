813919d16656089ecd33b9aa9770ed0e8d97730c
“Eric <enguyen2@yahoo.com>
2020-08-15 20:32:33 -0700

    add image in main screen | add background image | change buttons color | change CardView color

--- app/src/main/java/com/example/roomdbdemo/MainActivity.java
+++ app/src/main/java/com/example/roomdbdemo/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.roomdbdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_insert, btn_view;
+    ImageView stock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stock = findViewById(R.id.imageView);
+        stock.setImageResource(R.drawable.stock3);
 
         btn_insert = findViewById(R.id.btn_insert);
         btn_insert.setOnClickListener(new View.OnClickListener() {
